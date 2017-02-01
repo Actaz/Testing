4123b65e06d25285138cc0f6ee842b418384c958
Actaz <arvid.lundberg@hotmail.se>
2017-02-01 12:25:10 +0100

    Add files via upload

---
+++ Button.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Button extends JButton implements ActionListener{
+	
+	private String buttonText="";
+	private Color backC, fronC;
+	private int bx,by,bwidth,bheight;
+	private int default_x, default_y=20;
+	
+	
+	
+	public Button(){
+		this.setSize(default_x, default_y);
+		this.setActionListener();
+	}
+	public Button(int a, int b){
+		this.bx = a;
+		this.by = b;
+		this.setPreferredSize(new Dimension(bx,by));
+	}
+	public Button(String s){
+		this.setText(s);
+	}
+	public Button(String s, int a, int b){
+		this.bx = a;
+		this.by = b;
+		this.setText(s);
+		this.setPreferredSize(new Dimension(bx,by));
+	}
+
+	
+	public void setColor(Color foreground, Color background){
+		this.setBackground(background);
+		this.setForeground(foreground);
+		this.backC = background;
+		this.fronC = foreground;
+	}
+
+	
+	
+	private void setActionListener(){
+		this.addActionListener(this);
+	}
+	
+	
+	@Override
+	public void  actionPerformed(ActionEvent e){
+		
+	}
+}
---
+++ test4.java
@@ -0,0 +1,98 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class test4 extends JFrame implements ActionListener{
+	
+	private JButton b1;
+	
+	
+	public test4(){
+		createView();
+	
+		setTitle("Click me");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
+		setResizable(true);
+		
+		
+	}
+	public void createView(){
+		int size = 5;
+		JPanel panel = new JPanel(new GridBagLayout());
+		getContentPane().add(panel);
+		GridBagConstraints c = new GridBagConstraints();
+		
+		
+			//Adding panels onto the main panel
+			JPanel east = new JPanel(new GridBagLayout());
+			GridBagConstraints d = new GridBagConstraints();
+				d.gridx = 0;
+				d.gridy = 0;
+				d.ipady = 0;
+				east.add(new JButton("hello1"),d);
+				d.gridy = 1;
+				d.gridx = 0;
+				east.add(new Button("BANG",100,100),d);
+				d.gridy = 2;
+				d.gridx = 0;
+				east.add(new JButton("hello3"),d);
+			c.gridx = 0;
+			c.gridy = 0;
+			panel.add(east, c);
+			
+			
+			JPanel contents = new JPanel(new GridBagLayout());
+			GridBagConstraints cs = new GridBagConstraints();
+				cs.gridx = 0;
+				cs.gridy = 0;
+				for (int i = 0; i < size; i++) {
+					cs.gridy = 0;
+					for (int n = 0; n < size; n++){
+						b1 = new JButton();
+						b1.setPreferredSize(new Dimension(40,40));
+						b1.addActionListener(this);
+						contents.add(b1, cs);
+						cs.gridy += 1;
+					}
+					cs.gridx += 1;
+				}
+			c.gridx = 1;
+			c.gridy = 0;
+			panel.add(contents, c);
+			
+			JPanel syd = new JPanel(new GridBagLayout());
+			GridBagConstraints s = new GridBagConstraints();
+				s.gridx = 0;
+				s.gridy = 0;
+				s.ipady = 40;
+				syd.add(new JButton("hello1"),s);
+				s.gridy = 0;
+				s.gridx = 1;
+				syd.add(new JButton("hello2"),s);
+				s.gridy = 0;
+				s.gridx = 2;
+				syd.add(new JButton("hello3"),s);
+			c.gridwidth = 2;
+			c.gridx = 0;
+			c.gridy = 1;
+			panel.add(syd, c);
+	}
+
+
+	
+	public static void main(String[] args){
+		SwingUtilities.invokeLater(new Runnable(){
+			@Override
+			public void run(){
+				new test4().setVisible(true);;
+			}
+		});
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("wow");
+		
+	}
+}
